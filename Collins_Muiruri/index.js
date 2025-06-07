23948bf40c9d4590deb2b5ebd365c64e90f91829
Collins Muiruri <collinsmuiruri321@gmail.com>
2025-06-07 23:39:11 +0300

    Your commit message here

--- Collins_Muiruri/index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const hamburgerIcon = document.querySelector(".hamburger-icon");
-  const menuItems = document.querySelector(".menu-items");
-
-  hamburgerIcon.addEventListener("click", function () {
-    menuItems.classList.toggle("show-menu");
-  });
-});
