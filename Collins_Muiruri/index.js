7113ceaa76b9580978a585061c9be998a8b5d4ce
Collins Muiruri <collinsmuiruri321@gmail.com>
2025-06-07 22:31:31 +0300

    Your commit message here

--- Collins_Muiruri/index.js
+++ Collins_Muiruri/index.js
@@ -1,22 +1,8 @@
-function sendMessage() {
-    
-  const email = document.getElementById("email").value.trim();
-  const message = document.getElementById("message").value.trim();
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburgerIcon = document.querySelector(".hamburger-icon");
+  const menuItems = document.querySelector(".menu-items");
 
-  if (!email || !message) {
-    alert("Please fill in both the email and message fields.");
-    return;
-  }
-
-  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailPattern.test(email)) {
-    alert("Please enter a valid email address.");
-    return;
-  }
-
-  
-  alert("Message sent successfully!");
-
-  
-  document.querySelector("form").reset();
-}
+  hamburgerIcon.addEventListener("click", function () {
+    menuItems.classList.toggle("show-menu");
+  });
+});
